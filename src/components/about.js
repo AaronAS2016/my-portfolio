40ed711b9a179bff107d8547edaa8252d6f2fdf1
Aaron Saban <aaronsaban98@gmail.com>
2020-07-19 02:27:20 -0300

    added: about logic

--- src/components/about.js
+++ src/components/about.js
@@ -1,19 +1,123 @@
-import React from 'react';
+import React, {  useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { ButtonForm } from './button-form';
+
 
 const Wrapper = styled.div`
     width:100%;
-    display:flex;
-    justify-content:center;
-    flex-direction:column;
+    border-bottom: ${({theme}) => theme.main} 1px solid;
+    padding: 3em 4em;
+    position:relative;
+    box-sizing:border-box;
 
 `
 
 
+const SectionWrapper = styled.section`
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+`
+
+
+const BioTextWrapper = styled.div`
+
+`
+
+const BioTextForm = styled.form`
+    margin: 1em auto 3em;
+    max-widht:90vw;
+    width:300px;
+`
+
+const BioText = styled.p`
+    color: ${({theme}) => theme.text}; 
+    `
+    
+    
+    const SocialMediaWrapper = styled.div`
+    `
+    
+    
+    
+    
+    const BioImage = styled.div`
+    `
+
+    
+    /**
+     * Form
+     */
+    
+    
+    const FormField = styled.fieldset`
+    border:none;
+    padding:0;
+    `;
+    
+    const Legend = styled.legend`
+        color: ${({theme}) => theme.text}; 
+        text-transform:uppercase;
+        font-size:.75em;
+        letter-spacing:.15em;
+        margin:2px 0;
+    `
+
+    const BioModule = styled.div`
+        display:grid;
+        grid-template-columns:repeat(6, 1fr);
+        align:items:center;
+        grid-gap:2em;
+        margin:2em 0;
+    
+    `
+    
+
+
+const bioText = {
+    short: "",
+    shorter: "",
+    shortest: "",
+    long:"",
+    longer:" ",
+    longest:" "
+}    
+
 export const About = () => {
+
+    const [actualText, setText] = useState("");
+
+    useEffect(() => {
+        console.log(actualText)
+    }, [actualText])
+
+
     return (
-        <div>
-            About
-        </div>
+        <Wrapper>
+            <SectionWrapper>
+                <BioTextWrapper>
+                    <BioTextForm>
+                        <FormField>
+                            <Legend>Adjust bio lenght:</Legend>
+                            <BioModule>
+                                <ButtonForm handleClick={ setText }  isVisible={"true"} id="shortest" isSelected={false} text={"shortest"} name="lenght"/>
+                                <ButtonForm handleClick={ setText }  isVisible={"false"} id="shorter" isSelected={false} text={"shorter"} name="lenght"/>
+                                <ButtonForm handleClick={ setText }  isVisible={"false"} id="short" isSelected={false} text={"short"} name="lenght"/>
+                                <ButtonForm handleClick={ setText }  isVisible={"false"} id="long" isSelected={false} text={"long"} name="lenght"/>
+                                <ButtonForm handleClick={ setText }  isVisible={"false"} id="longer" isSelected={false} text={"longer"} name="lenght"/>
+                                <ButtonForm handleClick={ setText }  isVisible={"true"} id="longest" isSelected={false} text={"longest"} name="lenght"/>
+                            </BioModule>
+                        </FormField>
+
+
+                    </BioTextForm>
+                    <BioText>{ actualText }</BioText>
+                    <SocialMediaWrapper></SocialMediaWrapper>
+
+                </BioTextWrapper>
+                <BioImage></BioImage>
+            </SectionWrapper>
+        </Wrapper>
     )
 }
---
+++ src/components/button-form.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+
+const Wrapper = styled.div`
+    position:relative;
+    height:calc((300px - 10rem)/6 + 1rem);
+
+`
+
+
+const Input = styled.input`
+
+
+
+`
+
+
+const Label = styled.label`
+    position:absolute;
+    left:0;
+    clip:auto;
+    top:80%;
+
+`
+
+
+const Span = styled.span`
+
+    color:${({theme}) => theme.main };
+
+${props =>
+    props.isvisible === "true"
+      ? `
+          display: block;
+        `
+      : `
+          display:none;
+        `
+};
+
+`
+
+
+export const ButtonForm = ({ id, text, isSelected, name, isVisible, handleClick }) => {
+    return(
+        <Wrapper>
+            <Input type="radio" name={name} id={id} value={name} onClick={() => handleClick(id)}/>
+            
+            <Label htmlFor={id} >
+                <Span isvisible={isVisible}>
+                    {text}
+                </Span>
+            </Label>
+
+
+        </Wrapper>
+    )
+}
